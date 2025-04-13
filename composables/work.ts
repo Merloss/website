f97c06c3817d08f6f9395d9c210f38a6e9a13270
merloss <54235902+Merloss@users.noreply.github.com>
2025-04-13 19:32:47 +0300

    fix: typo

--- composables/work.ts
+++ composables/work.ts
@@ -5,7 +5,7 @@ export const WORK: Work[] = [
 		start_date: "2023/11/15",
 		end_date: "Present",
 		company: "Clemta",
-		location: "Delaware, USA",
+		location: "Delaware, U.S.A",
 		link: "https://clemta.com",
 		description_items: [
 			"Currently, I'm thriving as a backend developer at Clemta, where I dive deep into the complexities of system integration and the development of robust, scalable platforms using Go. My primary focus is on designing and building seamless technological solutions that empower entrepreneurs to launch and manage their businesses effortlessly. By prioritizing clean, maintainable code, I ensure that our platforms not only meet high technical standards but also deliver a reliable and intuitive experience for users.",
@@ -20,7 +20,7 @@ export const WORK: Work[] = [
 		start_date: "2023/03/09",
 		end_date: "2023/11/05",
 		company: "Voco",
-		location: "Ankara, Turkey",
+		location: "Ankara, Turkiye",
 		description_items: [
 			"Previously, I worked as a backend developer at Voco, an innovative startup that created a unique mobile application built with TypeScript. Voco was designed to connect people in close physical proximity who shared similar passions, such as hobbies, activities, or ideas, fostering real-world interactions. The platform utilized location-based notifications to allow users to discover and join nearby events—known as “Vocos”—organized by others in their area, making it incredibly easy to meet new people and build vibrant communities.",
 			"In my role, I focused on developing and optimizing the backend infrastructure that powered these real-time, location-driven features, ensuring a smooth and engaging user experience. I contributed to a scalable architecture that supported Voco’s mission of simplifying collaboration and connection. By maintaining clean, efficient code and embracing the startup’s tech-driven vibe, I helped create an environment where users could effortlessly form authentic relationships, bringing energy and innovation to the development process.",
